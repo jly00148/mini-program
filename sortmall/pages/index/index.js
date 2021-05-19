a2aa61931783a12b7776ab69bd31db3e097e1513
jly <453825818@qq.com>
2021-05-20 01:48:09 +0800

    render

--- sortmall/pages/index/index.js
+++ sortmall/pages/index/index.js
@@ -3,46 +3,36 @@
 const app = getApp()
 
 Page({
+  // 初始化数据绑定:
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    message:"hello 你好",
+    list:[
+      {id:1,task:'learn vue'},
+      {id:2,task:'learn react'}
+    ],
+    show:true
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  // 方法:
+  // 对应的事件处理函数，参数是event,通过event.currentTarget.dataset上的参数获取
+  tapView(event){
+    console.log(event) //是一个对象
+    // 获取参数id：
+    console.log(event.currentTarget.dataset.id)
+
+    // 获取参数age：
+    console.log(event.currentTarget.dataset.age)    
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+
+  handtap1(){
+      console.log('handtap1')
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
+
+  handtap2(){
+      console.log('handtap2')
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  
+  handtap3(){
+      console.log('handtap3')
+  }  
 })
