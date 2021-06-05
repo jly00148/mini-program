bdf5f06b7b1aa6d45b44c636a7a4cbf6745f859a
jly <453825818@qq.com>
2021-06-05 20:48:40 +0800

    online-play uni-app

---
+++ online-player-uniapp/App.vue
@@ -0,0 +1,37 @@
+<!-- 此文件为应用配置，用来配置App全局样式、监听以及应用生命周期 -->
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	
+	/* 引入官方样式库 */
+	@import url('./common/uni.css');
+	
+	/* 引入animate自定义图标库 */
+	@import url('./common/animate.css');	
+	
+	/* 引入free样式库 */
+	/* @import url('./common/free.css');	 */
+	
+	/* 引入全局样式库 */
+	@import url('./common/common.css');	
+	
+	/* 引入icon自定义图标库 */
+	/* @import url('./common/icon.css');	 */
+	
+
+	
+	
+</style>
---
+++ online-player-uniapp/pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+	<view class="content">
+		<image class="logo" src="/static/tabbar/logo.png"></image>
+		<view class="text-area">
+			<text class="title animated" hover-class="flash">{{title}},uniapp</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		},
+		// uni-app官网->生命周期->页面生命周期函数
+		onNavigationBarSearchInputChanged(){
+			console.log('搜索框内容变化')
+		},
+		onNavigationBarButtonTap(){
+			console.log('点击了按钮')
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
