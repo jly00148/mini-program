5b6ccf7b603a9e21c29099ce282c86aff1207369
jly <453825818@qq.com>
2021-06-21 17:04:33 +0800

    movies trailer

---
+++ Movie Trailer/pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item>
+				<view class="swiper-item"></view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item"></view>
+			</swiper-item>
+			<swiper-item>
+				<view class="swiper-item"></view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
