a0209fafd6c60ea9e32b907603ed57f4689a3318
jly <453825818@qq.com>
2021-06-23 11:53:27 +0800

    app-plus

--- Movie Trailer/pages/index/index.vue
+++ Movie Trailer/pages/index/index.vue
@@ -1,14 +1,28 @@
 <template>
 	<view>
-		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+		<!-- 轮播图 -->
+		<swiper 
+		:indicator-dots="true" 
+		:autoplay="true" 
+		:interval="3000" 
+		:duration="1000" 
+		:circular="true"
+		class="carousel"
+		>
 			<swiper-item>
-				<view class="swiper-item"></view>
+				<view class="swiper-item">
+					<image src="../../static/carousel/xjcy1.jpg" class="carousel"></image>
+				</view>
 			</swiper-item>
 			<swiper-item>
-				<view class="swiper-item"></view>
+				<view class="swiper-item">
+					<image src="../../static/carousel/mwdy1.jpg" class="carousel"></image>
+				</view>
 			</swiper-item>
 			<swiper-item>
-				<view class="swiper-item"></view>
+				<view class="swiper-item">
+					<image src="../../static/carousel/jgdzgsl1.jpg" class="carousel"></image>
+				</view>
 			</swiper-item>
 		</swiper>
 	</view>
